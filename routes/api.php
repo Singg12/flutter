104ca9d7a7f35875e84fa17337a11900e225f503
Promvuthy <promvuthy@PromVuthys-MacBook-Air.local>
2025-05-06 15:05:30 +0700

    api fetching

---
+++ routes/api.php
@@ -0,0 +1,44 @@
+<?php
+
+// use App\Http\Controllers\Api\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::apiResource('productapi', ProductController::class);
+//   Route::apiResource('products', ProductController::class);
+//  Route::get('/products', [ProductController::class, 'index']);
+ //Route::post('/products', [ProductController::class, 'store']);
+ //Route::put('/products/{product}', [ProductController::class, 'update']);
+
+//  Route::post('/products/upload', [ProductController::class, 'upload'])->name('api.products.upload');
+
+// Route::post('/register',[RegisteredUserController::class,'register']);
+
+// Route::get('/user', function (Request $request){
+
+//     return $request->user();
+
+// })->middleware('auth:sanctum');
+ Route::post('/products/upload', [ProductController::class, 'uploadImage']);
+
+//  Route::middleware('cors')->group(function () {
+//     Route::get('/endpoint', [ProductController::class, 'method']);
+// });
+// Route::apiResource('products', ProductController::class); //route to show product api
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
---
+++ storage/framework/views/25ed1b1ddacde099a44b54885a652818.php
@@ -0,0 +1,2 @@
+<body data-topbar="dark">
+<?php /**PATH /Users/promvuthy/Downloads/front-react-admin2/resources/views/layouts/body.blade.php ENDPATH**/ ?>
